ac0782654e1885ea1043401c68b4cd3a8c648de5
Michael Wender <michael@michaelwender.com>
2017-04-20 09:53:44 -0400

    Fixing search form and formatting for search results

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <!-- Search -->
 <form class="search" method="get" action="<?php echo home_url(); ?>">
-	<input class="search-input" type="text" placeholder="To search, type and hit enter.">
+	<input class="search-input" name="s" type="text" placeholder="To search, type and hit enter.">
 	<button class="search-submit" type="submit"><?php _e( 'Search', 'html5blank' ); ?></button>
 </form>
 <!-- /Search -->
